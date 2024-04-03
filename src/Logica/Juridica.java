bb74be54b39063394fe660f1079d43c376aa3665
AllenC <allenstivenc@gmail.com>
2024-04-03 10:51:00 -0500

    Creacion de los 2 tipos de clientes
    Creacion del cliente natural y juridico

---
+++ src/Logica/Juridica.java
@@ -0,0 +1,10 @@
+package Logica;
+
+public class Juridica extends Clientes{
+
+	public Juridica(int id, String nombre, String tipo) {
+		super(id, nombre, tipo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Logica/Natural.java
@@ -0,0 +1,10 @@
+package Logica;
+
+public class Natural extends Clientes{
+
+	public Natural(int id, String nombre, String tipo) {
+		super(id, nombre, tipo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
