435d4e58174713cb0f907da3b20c17336ea57305
AllenC <allenstivenc@gmail.com>
2024-04-03 11:09:02 -0500

    Modelo basico del menu
    manu de opciones

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,5 +1,54 @@
 package Menu;
 
-public class Menu {
+import java.util.Scanner;
 
+import Logica.Franquicia;
+
+public class Menu {
+	private Franquicia banco;
+	
+	public Menu() {
+		this.banco = new Franquicia();
+		this.presentacion();
+	}
+
+	public static void main(String[] args) {
+		new Menu();
+	}
+	
+	private void presentacion() {
+		Scanner sc = new Scanner(System.in);
+		int op=0;
+		do {
+			try {
+				System.out.println("Seleccione:");
+				System.out.println("0. Salir");
+				System.out.println("1. Crear Cliente");
+				System.out.println("2. Crear Producto");
+				System.out.println("3. Imprimir Clientes");
+				System.out.println("4. Imprimir Productos");
+				System.out.println("5. Alamcenar en archivo de texto plano");
+				System.out.println("6. Cargar desde archivo de texto plano");
+				op = sc.nextInt();
+				if(op == 1) {
+					this.crearCliente(sc);				
+				}else if(op == 2) {
+					this.creaCuenta(sc);
+				}else if(op == 3) {
+					this.imprimirClientes();
+				}else if(op == 4) {
+					this.imprimirCuentas();
+				}else if(op == 5) {
+					this.almacenar();
+				}else if(op == 6) {
+					this.cargar();
+				}
+			} catch (Exception e) {
+				e.getMessage();
+			}
+		}while (op!=0)
+		sc.close();
+	}
+	
+	
 }
